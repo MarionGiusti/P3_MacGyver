3d1d0f2918f404d0c06eb14d8a40d3d483f7bc69
Marion Giusti <m.giusti9@gmail.com>
2020-04-28 10:26:17 +0200

    Define the constants and the path of each images

---
+++ MGconstants.py
@@ -0,0 +1,23 @@
+""" Constants of the labyrinthe game : MacGyver's escape """
+
+# Parameters of the window to display
+nbr_sprite_side = 15
+size_sprite = 30
+size_window = nbr_sprite_side * size_sprite
+
+# Custamize the pygame window
+title_window = " MacGyver's escape "
+image_icone = "images/MacGyver.png"
+
+# Figures
+image_perso = "images/MacGyver.png"
+image_arrival = "images/watchman.png"
+
+# Survival items
+image_ether = "images/ether.png"
+image_needle = "images/needle.png"
+image_syringe = "images/syringe.png"
+
+
+
+
